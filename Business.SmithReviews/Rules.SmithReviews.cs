07e2b3b4938e534d20f2abf9b31e47942d0db181
Angus Crawford <gus.crawford@bell.net>
2017-03-05 04:37:35 -0500

    updated

---
+++ Business.SmithReviews/Rules.SmithReviews.cs
@@ -0,0 +1,25 @@
+﻿using Models.SmithReview;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.SmithReviews
+{
+    public static class Rules
+    {
+        public static class Review {
+            public static bool RatingIsBewtween1And5(ReviewModel item) {
+                if (item.Rating >= 1 && item.Rating <= 5)
+                    return true;
+                throw new RuleException();
+            }
+            public static bool CommentIsLongerThan4Characters(ReviewModel item) {
+                if (item.Comment.Length > 4)
+                    return true;
+                throw new RuleException();
+            }
+        }
+    }
+}
